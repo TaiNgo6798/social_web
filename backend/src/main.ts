fc3a4a79244eec128abc240d8c395e4aee50cf75
taingo <taingo6798@gmail.com>
2019-12-27 14:53:48 +0700

    alo alo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(6798);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
